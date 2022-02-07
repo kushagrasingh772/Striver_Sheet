0e65820395c1acd494505ad9ce942f92a4a675d4
Kushagra Singh <52888710+kushagrasingh772@users.noreply.github.com>
2022-02-08 01:40:00 +0530

    Add files via upload

---
+++ reverse_linkedlist.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode * prev=NULL;
+        ListNode* curr=head;
+        ListNode* next_p;
+
+        while (curr!=NULL)
+        {
+            next_p=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next_p;
+        }
+        
+        head=prev_p;
+        return head;
+    }
+}
+
+//better itrative
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *newHead = NULL;
+        while (head != NULL) {
+            ListNode *next = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+};
+
+//recursion
+
